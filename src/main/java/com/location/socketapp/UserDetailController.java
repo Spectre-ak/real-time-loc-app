d3cc878b25e0c9eef8aa3f3342681b1bc081ba34
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-07-02 17:46:46 +0530

    outline

--- src/main/java/com/location/socketapp/UserDetailController.java
+++ src/main/java/com/location/socketapp/UserDetailController.java
@@ -7,6 +7,7 @@ import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +17,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.azure.identity.DefaultAzureCredentialBuilder;
+import com.azure.security.keyvault.secrets.SecretClient;
+import com.azure.security.keyvault.secrets.SecretClientBuilder;
+import com.azure.security.keyvault.secrets.models.KeyVaultSecret;
+
 @RestController
 public class UserDetailController {
 
+	private String dburl=null;
+	
+	private String AuthenticatedUser(String type) {
+		if(dburl==null) {
+			getDBUrl();
+			//error retrieving the db url
+			if(dburl==null)return null;
+		}
+		
+		if(type.equals("1")) {
+			//log in
+		}
+		else if(type.equals("2")) {
+			//sign up
+		}
+		else {
+			//google
+			
+		}
+		
+		
+		return null;
+	}
+	
+	
+	
+	
 	@RequestMapping(value = "/loginDetail", method = RequestMethod.POST,
 			produces = MediaType.APPLICATION_JSON_VALUE)
 	
@@ -53,5 +86,46 @@ public class UserDetailController {
 	    
 
 	}
+	
+	@RequestMapping(value = "/signingoogle",method = RequestMethod.POST,
+			produces = MediaType.APPLICATION_JSON_VALUE)
+	public Object signingoogle(HttpServletResponse response,
+			HttpServletRequest request, @RequestBody HashMap<String,String> data) throws IOException {
+
+		System.out.println(data);
+	    response.addCookie(new Cookie("heroku-nav-data", "adad"));
+	    HashMap<String,String> responseHashMap=new HashMap<>();
+	    responseHashMap.put("res","valid");
+	    //return new ResponseEntity<String>("msg",HttpStatus.OK); 
+	    return new ResponseEntity<HashMap<String,String>>(responseHashMap, HttpStatus.OK);
+	    
+
+	}
+	
+	private void getDBUrl() {
+		
+		if(true) {
+			//locally testing
+			dburl=dbcon.dburl;return;
+		}
+		
+		try {
+			String keyVaultName = "keyVaultsForSecureCreds";
+			String keyVaultUri = "https://" + keyVaultName + ".vault.azure.net";
+
+			SecretClient secretClient = new SecretClientBuilder()
+			    .vaultUrl(keyVaultUri)
+			    .credential(new DefaultAzureCredentialBuilder().build())
+			    .buildClient();
+			
+			 KeyVaultSecret retrievedSecret = secretClient.getSecret("databaseCredentials");
+			 this.dburl=retrievedSecret.getValue();
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	
+	
 	
 }
