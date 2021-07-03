53f6a641ab3dd6a31e94b665c21f2a0130d24029
Spectre-ak <62694340+Spectre-ak@users.noreply.github.com>
2021-07-04 01:20:33 +0530

    Update SocketAppApplication.java

--- src/main/java/com/location/socketapp/SocketAppApplication.java
+++ src/main/java/com/location/socketapp/SocketAppApplication.java
@@ -48,38 +48,39 @@ import org.apache.http.message.BasicNameValuePair;
 @SpringBootApplication
 public class SocketAppApplication {
 
-	static void test1() throws Exception{
-		//Creating a HttpClient object
-	      CloseableHttpClient httpclient = HttpClients.createDefault();
-
-	      //Creating a HttpGet object
-	      HttpGet httpget = new HttpGet("https://www.google.com/search?q=akash");
-
-	      //Printing the method used
-	      System.out.println("Request Type: "+httpget.getMethod());
-
-	      //Executing the Get request
-	      HttpResponse httpresponse = httpclient.execute(httpget);
-
-	      Scanner sc = new Scanner(httpresponse.getEntity().getContent());
-
-	      //Printing the status line
-	      System.out.println(httpresponse.getStatusLine());
-	      String totalString="";
-	      while(sc.hasNextLine()) {
-	         totalString+=sc.nextLine();
-	      }
-	      System.out.println();
-	      System.out.println(totalString.length());
-	      try {
-			FileWriter fwFileWriter=new FileWriter(new File("te.html"));
-			fwFileWriter.write(totalString);
-			fwFileWriter.close();
-		} catch (Exception e) {
-			// TODO: handle exception
-			e.printStackTrace();
-		}
-	}
+//	static void test1() throws Exception{
+//		//Creating a HttpClient object
+//	      CloseableHttpClient httpclient = HttpClients.createDefault();
+//
+//	      //Creating a HttpGet object
+//	      HttpGet httpget = new HttpGet("https://www.google.com/search?q=akash");
+//
+//	      //Printing the method used
+//	      System.out.println("Request Type: "+httpget.getMethod());
+//
+//	      //Executing the Get request
+//	      HttpResponse httpresponse = httpclient.execute(httpget);
+//
+//	      Scanner sc = new Scanner(httpresponse.getEntity().getContent());
+//
+//	      //Printing the status line
+//	      System.out.println(httpresponse.getStatusLine());
+//	      String totalString="";
+//	      while(sc.hasNextLine()) {
+//	         totalString+=sc.nextLine();
+//	      }
+//	      System.out.println();
+//	      System.out.println(totalString.length());
+//	      try {
+//			FileWriter fwFileWriter=new FileWriter(new File("te.html"));
+//			fwFileWriter.write(totalString);
+//			fwFileWriter.close();
+//		} catch (Exception e) {
+//			// TODO: handle exception
+//			e.printStackTrace();
+//		}
+//	}
+//	
 	public static void main(String[] args) throws Exception{
 		//name();if(true)return;
 		SpringApplication.run(SocketAppApplication.class, args);
@@ -126,62 +127,62 @@ public class SocketAppApplication {
 		
 	}
 	
-	public static void name() {
-		ConnectionString connectionString = new 
-				ConnectionString(dbcon.dburl);
-		MongoClientSettings settings = MongoClientSettings.builder()
-		        .applyConnectionString(connectionString)
-		        .build();
-		MongoClient mongoClient = MongoClients.create(settings);
-		MongoDatabase database = mongoClient.getDatabase("mongoDatabase0");
-
-		Random rand = new Random();
-		Document student = new Document("_id", new ObjectId());
-		
-		student.append("student_id", 10000d)
-        .append("class_id", 1d)
-        .append("scores", asList(new Document("type", "exam").append("score", rand.nextDouble() * 100),
-                                 new Document("type", "quiz").append("score", rand.nextDouble() * 100),
-                                 new Document("type", "homework").append("score", rand.nextDouble() * 100),
-                                 new Document("type", "homework").append("score", rand.nextDouble() * 100)));
-
-		
-		database.getCollection("collection0").insertOne(student);
-		
-	
-		MongoCollection<Document> collection = database.getCollection("collection0");
-		
-		FindIterable<Document> fi = collection.find();
-        MongoCursor<Document> cursor = fi.iterator();
-        try {
-            while(cursor.hasNext()) {               
-                Document document=cursor.next();
-                System.out.println(document.get("_id"));
-            }
-        } finally {
-            cursor.close();
-        }
-		
-		
-		Document query = new Document("_id", new ObjectId("60db49c74c9a4324fb978112"));
-		Document result = collection.find(query).iterator().next();
-		result.append("pos","above horizon");
-		System.out.println(result.getString("level"));
-		
-		ConcurrentHashMap<String,String> concurrentHashMap=new ConcurrentHashMap<String, String>();
-		
-	}
-	
-	public static void tasks() {
-		Runnable runnable = new Runnable() {
-		      public void run() {
-		        // task to run goes here
-		        System.out.println("Hello !!");
-		      }
-		    };
-		    ScheduledExecutorService service = Executors
-		                    .newSingleThreadScheduledExecutor();
-		    service.scheduleAtFixedRate(runnable, 0, 5, TimeUnit.SECONDS);
-	}
+//	public static void name() {
+//		ConnectionString connectionString = new 
+//				ConnectionString(dbcon.dburl);
+//		MongoClientSettings settings = MongoClientSettings.builder()
+//		        .applyConnectionString(connectionString)
+//		        .build();
+//		MongoClient mongoClient = MongoClients.create(settings);
+//		MongoDatabase database = mongoClient.getDatabase("mongoDatabase0");
+//
+//		Random rand = new Random();
+//		Document student = new Document("_id", new ObjectId());
+//		
+//		student.append("student_id", 10000d)
+//        .append("class_id", 1d)
+//        .append("scores", asList(new Document("type", "exam").append("score", rand.nextDouble() * 100),
+//                                 new Document("type", "quiz").append("score", rand.nextDouble() * 100),
+//                                 new Document("type", "homework").append("score", rand.nextDouble() * 100),
+//                                 new Document("type", "homework").append("score", rand.nextDouble() * 100)));
+//
+//		
+//		database.getCollection("collection0").insertOne(student);
+//		
+//	
+//		MongoCollection<Document> collection = database.getCollection("collection0");
+//		
+//		FindIterable<Document> fi = collection.find();
+//        MongoCursor<Document> cursor = fi.iterator();
+//        try {
+//            while(cursor.hasNext()) {               
+//                Document document=cursor.next();
+//                System.out.println(document.get("_id"));
+//            }
+//        } finally {
+//            cursor.close();
+//        }
+//		
+//		
+//		Document query = new Document("_id", new ObjectId("60db49c74c9a4324fb978112"));
+//		Document result = collection.find(query).iterator().next();
+//		result.append("pos","above horizon");
+//		System.out.println(result.getString("level"));
+//		
+//		ConcurrentHashMap<String,String> concurrentHashMap=new ConcurrentHashMap<String, String>();
+//		
+//	}
+//	
+//	public static void tasks() {
+//		Runnable runnable = new Runnable() {
+//		      public void run() {
+//		        // task to run goes here
+//		        System.out.println("Hello !!");
+//		      }
+//		    };
+//		    ScheduledExecutorService service = Executors
+//		                    .newSingleThreadScheduledExecutor();
+//		    service.scheduleAtFixedRate(runnable, 0, 5, TimeUnit.SECONDS);
+//	}
 
 }
